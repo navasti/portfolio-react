839224b83da98d71ab548076728d435c7f821eb4
Pawel Szostak <szostak_pawel@outlook.com>
2020-01-08 11:43:25 +0100

    Updates

---
+++ precache-manifest.da87d3c12409efd26a8cbcec14d24a8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2abbbf3bdf109a14c54043e3d42adef1",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "930c9b4d8c3c41dfc33a",
+    "url": "/portfolio-react/static/css/main.9b7a4bf2.chunk.css"
+  },
+  {
+    "revision": "bb07d5f7d2af59cde2fc",
+    "url": "/portfolio-react/static/js/2.6575803e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio-react/static/js/2.6575803e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "930c9b4d8c3c41dfc33a",
+    "url": "/portfolio-react/static/js/main.22edc7cd.chunk.js"
+  },
+  {
+    "revision": "f82b4c39403e02d3815c",
+    "url": "/portfolio-react/static/js/runtime-main.d3484070.js"
+  },
+  {
+    "revision": "ba8c6b5a5157a9b8a4ae44a2934a7935",
+    "url": "/portfolio-react/static/media/bgc.ba8c6b5a.JPG"
+  }
+]);
